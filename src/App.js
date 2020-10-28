94a1de422810c6c7ee79ee9c25e33342e4bf9b41
Athul <athulsn2@gmail.com>
2020-10-28 19:13:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img style={{objectFit:'none', height:'100%', background: 'inherit'}} src="under_dev.png"/>
+      <img style={{objectFit:'none', height:'100%', background: 'inherit'}} src="under_dev.svg"/>
     </div>
   );
 }
